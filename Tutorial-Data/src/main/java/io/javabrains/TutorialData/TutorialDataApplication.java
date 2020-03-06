48a316b0e6f77fb5ca78b43257813272876cf65b
Brandon Dabrowski <bdabrowski1997@gmail.com>
2020-03-06 11:05:11 -0500

    more tutorial stuff, working with databases and maven

---
+++ Tutorial-Data/src/main/java/io/javabrains/TutorialData/TutorialDataApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.TutorialData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialDataApplication.class, args);
+	}
+
+}
