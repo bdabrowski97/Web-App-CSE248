7a97608e99ccfe056436e98eb69616aae3962a39
Brandon Dabrowski <bdabrowski1997@gmail.com>
2020-03-28 20:25:17 -0400

    fixing github files

--- BasicWebApp2/src/main/java/com/brandon/BasicWebApp2/dao/AccountRepo.java
+++
@@ -1,8 +0,0 @@
-package com.brandon.BasicWebApp2.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import com.brandon.BasicWebApp2.model.Account;
-
-public interface AccountRepo extends CrudRepository<Account, String> {
-
-}
--- BasicWebApp2/src/main/java/com/brandon/BasicWebApp2/model/TestClass.java
+++
@@ -1,8 +0,0 @@
-package com.brandon.BasicWebApp2.model;
-
-public class TestClass {
-	
-	public static String capsMsg(String string) {
-		return string.toUpperCase();
-	}
-}
