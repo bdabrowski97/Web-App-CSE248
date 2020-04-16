92f0deb9ac8768141aea86ec97fbc2771ab2cf33
Brandon Dabrowski <bdabrowski1997@gmail.com>
2020-04-16 10:48:59 -0400

    removed tutorial projects for less clutter

--- Basic-Web-App/src/main/java/model/msgRepo.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface msgRepo extends CrudRepository<Message, Integer> {
-
-}
--- Tutorial-Data/src/main/java/io/javabrains/TopicRepository.java
+++
@@ -1,7 +0,0 @@
-package io.javabrains;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic,String> {
-
-}
